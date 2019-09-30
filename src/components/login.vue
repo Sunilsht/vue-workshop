4205e5e33d32baf5e508108aa40b28ee08872bb3
sunil shrestha <explore.sunilshrestha@gmail.com>
2019-09-30 17:00:45 +0545

    net

---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    <div class="container">
+      <div class="overlay-container">
+        <div class="overlay">
+          <div class="overlay-left">
+            <h2>wellcome back</h2>
+            <p>please login with your persional infp</p>
+            <button>sign in</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
